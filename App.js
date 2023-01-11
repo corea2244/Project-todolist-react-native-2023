ea3782c994f3fd1b38f30bee0df3ff1123d948bc
corea2244 <59365194+corea2244@users.noreply.github.com>
2023-01-11 23:01:27 +0900

    [part2-ch3-8-투두 (2) 추가/삭제/체크 UI 개발, FlatList, KeyboardAvo…

--- App.js
+++ App.js
@@ -2,53 +2,31 @@ import dayjs from "dayjs";
 import { useEffect } from "react";
 import {
   FlatList,
-  SafeAreaView,
+  Image,
+  KeyboardAvoidingView,
   StyleSheet,
   Text,
-  TouchableOpacity,
   View,
+  Platform,
+  Pressable,
+  TouchableOpacity,
+  Keyboard,
 } from "react-native";
-import {
-  getStatusBarHeight,
-  getBottomSpace,
-} from "react-native-iphone-x-helper";
-import Margin from "./src/Margin";
-import { runPracticeDayjs } from "./src/practice-dayjs";
-import { getCalendarColumns, getDayColor, getDayText } from "./src/util";
-import { SimpleLineIcons } from "@expo/vector-icons";
+
 import DateTimePickerModal from "react-native-modal-datetime-picker";
+import { Ionicons } from "@expo/vector-icons";
+import { runPracticeDayjs } from "./src/practice-dayjs";
+import {
+  bottomSpace,
+  getCalendarColumns,
+  ITEM_WIDTH,
+  statusBarHeight,
+} from "./src/util";
 import { usecalendar } from "./src/hook/use-calendar";
-
-const columnSize = 30;
-const statusBarHeight = getStatusBarHeight(true);
-const bottomSpace = getBottomSpace();
-
-const Column = ({ text, color, opacity, disabled, onPress, isSelected }) => {
-  return (
-    <TouchableOpacity
-      disabled={disabled}
-      onPress={onPress}
-      style={{
-        width: columnSize,
-        height: columnSize,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: isSelected ? "#c2c2c2" : "transparent",
-        borderRadius: columnSize / 2,
-      }}
-    >
-      <Text style={{ color, opacity }}>{text}</Text>
-    </TouchableOpacity>
-  );
-};
-
-const ArrowButton = ({ onPress, iconName }) => {
-  return (
-    <TouchableOpacity onPress={onPress} style={{ padding: 15 }}>
-      <SimpleLineIcons name={iconName} size={15} color="#404040" />
-    </TouchableOpacity>
-  );
-};
+import { useTodoList } from "./src/hook/use-todo-list";
+import Calendar from "./src/Calendar";
+import Margin from "./src/Margin";
+import AddTodoInput from "./src/AddTodoInput";
 
 export default function App() {
   const now = dayjs();
@@ -62,92 +40,115 @@ export default function App() {
     subtract1Month,
     addtract1Month,
   } = usecalendar(now);
+  const { todoList, input, setInput } = useTodoList(selectedDate);
 
   const columns = getCalendarColumns(selectedDate);
   const onPressLeftArrow = subtract1Month;
   const onPressRightArrow = addtract1Month;
+  const onPressheaderDate = showDatePicker;
+  const onPressDate = setSelectedDate;
 
   const ListHeaderComponent = () => {
-    const currentDateText = dayjs(selectedDate).format("YYYY.MM.DD");
     return (
       <View>
+        <Calendar
+          columns={columns}
+          selectedDate={selectedDate}
+          onPressLeftArrow={onPressLeftArrow}
+          onPressRightArrow={onPressRightArrow}
+          onPressHeaderDate={onPressheaderDate}
+          onPressDate={onPressDate}
+        />
         <Margin height={15} />
         <View
           style={{
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
+            width: 4,
+            height: 4,
+            borderRadius: 4 / 2,
+            backgroundColor: "#a3a3a3",
+            alignSelf: "center",
           }}
-        >
-          <ArrowButton iconName="arrow-left" onPress={onPressLeftArrow} />
-          <TouchableOpacity onPress={showDatePicker}>
-            <Text style={{ fontSize: 20, color: "#404040" }}>
-              {currentDateText}
-            </Text>
-          </TouchableOpacity>
-          <ArrowButton iconName="arrow-right" onPress={onPressRightArrow} />
-        </View>
+        ></View>
         <Margin height={15} />
-        <View style={{ flexDirection: "row" }}>
-          {[0, 1, 2, 3, 4, 5, 6].map((day) => {
-            const dayText = getDayText(day);
-            const color = getDayColor(day);
-            return (
-              <Column
-                key={`day-${day}`}
-                text={dayText}
-                color={color}
-                opacity={1}
-                disabled={true}
-              />
-            );
-          })}
-        </View>
       </View>
     );
   };
 
-  const RenderItem = ({ date }) => {
-    const dateText = dayjs(date).get("date");
-    const day = dayjs(date).get("day");
-    const color = getDayColor(day);
-    const isCurrentMonth = dayjs(date).isSame(selectedDate, "month");
-    const onPress = () => {
-      setSelectedDate(date);
-    };
-    const isSelected = dayjs(date).isSame(selectedDate, "date");
-
+  const renderItem = ({ item: todo }) => {
+    const isSuccess = todo.isSuccess;
     return (
-      <Column
-        text={dateText}
-        color={color}
-        opacity={isCurrentMonth ? 1 : 0.4}
-        onPress={onPress}
-        isSelected={isSelected}
-      />
+      <View
+        style={{
+          width: ITEM_WIDTH,
+          alignSelf: "center",
+          paddingVertical: 10,
+          paddingHorizontal: 5,
+          borderBottomWidth: 0.2,
+          borderColor: "#a6a6a6",
+          flexDirection: "row",
+        }}
+      >
+        <Text style={{ flex: 1, fontSize: 14, color: "#595959" }}>
+          {todo.content}
+        </Text>
+        <Ionicons
+          name="ios-checkmark"
+          size={17}
+          color={isSuccess ? "#595959" : "#bfbfbf"}
+        />
+      </View>
     );
   };
+
+  const onPressAdd = () => {};
 
   useEffect(() => {
     runPracticeDayjs();
   }, []);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={columns}
-        keyExtractor={(_, index) => `column-${index}`}
-        numColumns={7}
-        renderItem={({ item }) => <RenderItem date={item} />}
-        ListHeaderComponent={ListHeaderComponent}
+    <Pressable style={styles.container} onPress={Keyboard.dismiss}>
+      <Image
+        source={{
+          // 출처: https://kr.freepik.com/free-photo/white-crumpled-paper-texture-for-background_1189772.htm
+          uri: "https://img.freepik.com/free-photo/white-crumpled-paper-texture-for-background_1373-159.jpg?w=1060&t=st=1667524235~exp=1667524835~hmac=8a3d988d6c33a32017e280768e1aa4037b1ec8078c98fe21f0ea2ef361aebf2c",
+        }}
+        style={{
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+        }}
       />
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <>
+          <FlatList
+            data={todoList}
+            ListHeaderComponent={ListHeaderComponent}
+            renderItem={renderItem}
+            style={{ paddingTop: statusBarHeight }}
+          />
+
+          <AddTodoInput
+            value={input}
+            onChangeText={setInput}
+            placeholder={`${dayjs(selectedDate).format("MM.D")}에 추가할 투두`}
+            onPressAdd={null}
+          />
+        </>
+      </KeyboardAvoidingView>
+
+      <Margin height={bottomSpace} />
+
       <DateTimePickerModal
         isVisible={isDatePickerVisible}
         mode="date"
         onConfirm={handleConfirm}
         onCancel={hideDatePicker}
       />
-    </SafeAreaView>
+    </Pressable>
   );
 }
 
@@ -157,7 +158,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    paddingTop: statusBarHeight,
-    paddingBottom: bottomSpace,
   },
 });
---
+++ src/AddTodoInput.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { TextInput, TouchableOpacity, View } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { bottomSpace, ITEM_WIDTH } from "./util";
+export default ({ value, onChangeText, placeholder, onPressAdd }) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        width: ITEM_WIDTH,
+        alignItems: "center",
+      }}
+    >
+      <TextInput
+        value={value}
+        onChangeText={onChangeText}
+        placeholder={placeholder}
+        style={{ flex: 1, padding: 5, color: "#595959" }}
+      />
+      <TouchableOpacity onPress={onPressAdd} style={{ padding: 5 }}>
+        <AntDesign name="plus" size={18} color="#595959" />
+      </TouchableOpacity>
+    </View>
+  );
+};
